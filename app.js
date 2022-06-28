6e1c6926cc4c0a4c56b7dbb695b253a76b84023f
SELab21-group-14 <93638824+SELab21-group-14@users.noreply.github.com>
2022-06-28 17:56:44 +0530

    Added database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose= require("mongoose");
 const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -17,19 +18,33 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const Posts=[]
-const post={
-  title:"Home",
-  content:homeStartingContent
-}
-Posts.push(post)
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+// const Posts=[]
+// const post={
+//   title:"Home",
+//   content:homeStartingContent
+// }
+// Posts.push(post)
 
 
 
 
 app.get("/",function(req,res)
 {
-  res.render("home",{Posts:Posts})
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
 })
 
 app.get("/about",function(req,res)
@@ -52,30 +67,28 @@ app.post("/compose",function(req,res)
 {
   content=req.body.postContent;
   title=req.body.postTitle;
-  const post={
+  const post=new Post({
     title:title,
     content:content
-  }
-  Posts.push(post)
-  res.redirect("/")
+  })
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
 
 })
 
-app.get("/post/:postName",function(req,res)
+app.get("/post/:postId",function(req,res)
 {
-  routeName=req.params.postName;
-  Posts.forEach(function(e)
-  {
-    if(e.title===routeName)
-    {
-      const p={
-        title:e.title,
-        content:e.content
-      }
-      res.render("post",{post:p})
-    }
-  })
-  res.redirect("/")
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
  
  
 
